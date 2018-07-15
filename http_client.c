7b64e718e66d0c465b431c0335760d90dbd7c975
Ajay Randhawa <30903923+ajayrandhawa@users.noreply.github.com>
2018-07-15 11:28:02 +0530

    Upload Project Files

---
+++ http_client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]) {
+
+	char *address;
+	address = argv[1];
+
+	int client_socket;
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// connect to an address
+	struct sockaddr_in remote_address;
+	remote_address.sin_family = AF_INET;
+	remote_address.sin_port = htons(80);
+	inet_aton(address, &remote_address.sin_addr.s_addr);
+
+	connect(client_socket, (struct sockaddr *) &remote_address, sizeof(remote_address));
+
+	char request[] = "GET / HTTP/1.1\r\n\r\n";
+	char response[4096];
+
+	send(client_socket, request, sizeof(request), 0);
+	recv(client_socket, &response, sizeof(response), 0);
+
+	printf("response from the server: %s\n", response);
+	close(client_socket);
+
+	return 0;
+
+
+}
